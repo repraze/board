bffc1f8bcda9be58b82bf9dd8232b6fb09b5057b
repraze <repraze@gmail.com>
2019-01-06 22:26:23 +0000

    Uses symbols

--- core/valid.js
+++ core/valid.js
@@ -1,73 +1,65 @@
-const check = Symbol('func');
-const func = Symbol('func');
+const SYMB_CHECK = Symbol('check');
+const SYMB_FUNC = Symbol('func');
+const SYMB_VALIDATOR = Symbol('validator');
 
 const number = {
     validator : (value)=>(typeof value === 'number'),
-    [check] : {
+    [SYMB_CHECK] : {
         integer : (value)=>(value === (value | 0)),
     },
-    [func] : {
+    [SYMB_FUNC] : {
         gt : (v)=>(value)=>(value > v),
         lt : (v)=>(value)=>(value < v),
     }
 };
 
-const validators = {
+const TYPES = {
     number,
 }
 
-function runner(value, validator){
-    for(let i = 0; i < validator.tests.length; ++i){
-        const test = validator.tests[i];
-        const pass = test(value);
-        if(!pass){
-            const type = validator.type
-            if(i === 0){
-                throw new Error(`Expected \`${value}\` to be of type \`${type}\`, but received type \`${typeof value}\``);
-            }else{
-                throw new Error(`Expected ${type} \`${value}\` to pass \`${test.key}\``);
+const valid = new Proxy(function(){}, {
+    apply : function(target, thisArg, args){
+        const value = args[0];
+        const validator = args[1][SYMB_VALIDATOR];
+        for(let i = 0; i < validator.tests.length; ++i){
+            const test = validator.tests[i];
+            const pass = test(value);
+            if(!pass){
+                const type = validator.type;
+                if(i === 0){
+                    throw new Error(`Expected \`${value}\` to be of type \`${type}\`, but received type \`${typeof value}\``);
+                }else{
+                    throw new Error(`Expected ${type} \`${value}\` to pass \`${test.key}\``);
+                }
             }
         }
-    }
-}
-
-class Validator{
-    constructor(type){
-        this.type = type;
-        this.tests = [];
-    }
-    add(test){
-        this.tests.push(test);
-    }
-}
-
-const valid = new Proxy(runner, {
-    get : function(obj, key){
-        if(!(key in validators)){
+    },
+    get : function(target, key){
+        if(!(key in TYPES)){
             throw new Error(`invalid type \`${key}\` given`);
         }
-        const type = validators[key];
-        const validator = new Validator(key);
-        validator.add(type.validator);
+        const type = TYPES[key];
+        const validator = {
+            type : key,
+            tests : [type.validator]
+        };
 
         const proxy = new Proxy(validator, {
             get : function(validator, key){
-                if(['type', 'tests'].includes(key)){
-                    return validator[key];
-                }else if(key in type[check]){
+                if(key === SYMB_VALIDATOR){
+                    return validator;
+                }else if(key in type[SYMB_CHECK]){
                     const name = `${key}`;
-                    const test = type[check][key];
+                    const test = type[SYMB_CHECK][key];
                     test.key = name;
-                    validator.add(test);
-
+                    validator.tests.push(test);
                     return proxy;
-                }else if(key in type[func]){
+                }else if(key in type[SYMB_FUNC]){
                     return (...args)=>{
                         const name = `${key}(${[...args].join(', ')})`;
-                        const test = type[func][key](...args);
+                        const test = type[SYMB_FUNC][key](...args);
                         test.key = name;
-                        validator.add(test);
-
+                        validator.tests.push(test);
                         return proxy;
                     };
                 }else{
